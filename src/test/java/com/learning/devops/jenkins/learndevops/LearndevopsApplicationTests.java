a88683b36f4d3abfd2013baab3d1e3bb892b6035
KarthicSanthanam <karthic.santhanam@gmail.com>
2021-12-23 20:45:04 +0530

    Initial commit of source code.

---
+++ src/test/java/com/learning/devops/jenkins/learndevops/LearndevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.devops.jenkins.learndevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearndevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
